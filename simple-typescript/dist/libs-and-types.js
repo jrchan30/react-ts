45a974bbf205ff9d50731c9832e5cb54bd167b98
Russell Chan <jrussellchan2000@gmail.com>
2022-05-05 22:58:15 +0700

    intersection, type alias, libraries and d.ts

---
+++ simple-typescript/dist/libs-and-types.js
@@ -0,0 +1,4 @@
+"use strict";
+// npm i -D @types/npm-package-name
+Object.defineProperty(exports, "__esModule", { value: true });
+// React.someFn()
---
+++ simple-typescript/src/libs-and-types.ts
@@ -0,0 +1,6 @@
+// npm i -D @types/npm-package-name
+
+import ApolloClient from 'apollo-client';
+import React from 'react';
+
+// React.someFn()
